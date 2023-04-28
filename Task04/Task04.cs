b1569177566c35dc20fa1c7605dfee054062ef5c
Ramil <ramil.zhimoldinov@outlook.com>
2023-04-28 18:12:03 +0300

    write program for task04 with random

--- Task04/Task04.cs
+++ Task04/Task04.cs
@@ -5,3 +5,26 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
+int number1 = new Random().Next();
+Console.WriteLine($"Первое случайное число: {number1}");
+
+int number2 = new Random().Next();
+Console.WriteLine($"Второе случайное число: {number2}");
+
+int number3 = new Random().Next();
+Console.WriteLine($"Третье случайное число: {number3}");
+
+if (number1 > number2 && number1 > number3)
+{
+    Console.WriteLine($"Максимальное число = {number1}");
+}
+
+else if (number2 > number1 && number2 > number3)
+{
+    Console.WriteLine($"Максимальное число = {number2}");
+}
+
+else
+{
+    Console.WriteLine($"Максимальное число = {number3}");
+}
